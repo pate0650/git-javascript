373d136153782fbd5e486e803c7de5ae02b0ef2f
GArix18 <pate0650@algonquinlive.com>
2020-01-09 18:13:08 -0500

    made changes to js file

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const name = 'Patel Gary'
 console.log(`${name}`)
 
 const number = 4
-console.log(`my number is  ${number} .the result is = ${number} * 2 * 5 / ${number} - 7 .`)
+console.log(`my number is  ${number} .the result is = ${number * 2 * 5 / number - 7} .`)
