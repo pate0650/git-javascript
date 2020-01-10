31f74ca0eb3c033e563d4149cad3ad08d53b5702
GArix18 <pate0650@algonquinlive.com>
2020-01-10 10:57:38 -0500

    changes in js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const name = 'Patel Gary'
-console.log(`${name}`)
+console.log(`Hello my name is ${name} .`)
 
 const number = 4
 console.log(`my number is  ${number} .the result is = ${number * 2 * 5 / number - 7} .`)
